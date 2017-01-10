f475c285f88291847f454b422b9fca7941acf005
student <student@STEP.LAN>
2017-01-10 21:02:04 +0200

    end

--- JavaScript/lesson 1/alert.js
+++ JavaScript/lesson 1/alert.js
@@ -1,1 +1,12 @@
-alert("Hello world")
+// var test=a=b=query=40;
+// var test2=61;
+// var test3;
+// test3=test+test2;
+// var i=2;
+// b=i++;
+// alert(b);
+// c=++i;
+// alert(c);
+
+var adult=prompt(confirm("is you adult?"));
+var age=prompt("what is your age?","18");
