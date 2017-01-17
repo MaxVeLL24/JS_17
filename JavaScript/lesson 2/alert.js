aa27a453da0ef03da897d68ee861c702e612aeb7
Максим Павленко < >
2017-01-17 13:25:52 +0200

    add_lessons

---
+++ JavaScript/lesson 2/alert.js
@@ -0,0 +1,87 @@
+// var test=a=b=query=40;
+// var test2=61;
+// var test3;
+// test3=test+test2;
+// var i=2;
+// b=i++;
+// alert(b);
+// c=++i;
+// alert(c);
+
+// var isadult = confirm("Is you adult?");
+// if (isadult) {
+//     alert("welcome")
+// }
+// else if (!isadult) {
+//     alert("Sorry...")
+// }
+// var age = prompt("what is your age?", "18");
+//
+// if (age > 45) {
+//     alert("welcome old man")
+// }
+// else if (age > 30) {
+//     alert("welcome mister")
+// }
+// else if (age > 18) {
+//     alert("Welcome yong man")
+// }
+// else if (age > 18) {
+//     alert("sorry, you are too young")
+// }
+
+// var login = prompt("What is your login name");
+// if (!login) {
+//     alert("Enter canceled")
+// }
+// else {
+//     if (login == "admin") {
+//         var pass = prompt("Your password?")
+//         if (!pass) {
+//             alert("Enter canceled")
+//         }
+//         else if (pass == "123123") {
+//             alert("welcome")
+//         }
+//         else {
+//             alert("Your password isn't correct")
+//         }
+//     }
+//     else
+//         alert("no such user")
+// }
+// var a = prompt("Set number  #1");
+// var b = prompt("Set number  #2");
+// var c = prompt("Set number  #3");
+//
+// if (a > b) {
+//     if (a > c) {
+//         alert(a)
+//     }
+//     else {
+//         alert(c)
+//     }
+// }
+// if (a < b) {
+//     if (b > c) {
+//         alert(b)
+//     }
+//     else {
+//         alert(c)
+//     }
+// }
+var age = prompt("What is your age");
+if (!age) {
+    alert("Enter canceled")
+}
+if (age==)
+else {
+    if (age > 18) {
+        var name = prompt("What is your name?");
+        var job = prompt("What is your job?");
+    }
+    else if (age < 18) {
+        var name = prompt("What is your name7");
+        var stud = prompt("Where do you study7");
+    }
+}
---
+++ JavaScript/lesson 3/alert.js
@@ -0,0 +1,63 @@
+// var hov = prompt("how much is 2+2?");
+// switch (hov) {
+//     case("4"):
+//         alert("You are right");
+//         break;
+//     case ("3"):
+//         alert("too small number");
+//         break;
+//     case ("5"):
+//         alert("too big number");
+//         break;
+//     default:
+//         alert("Wrong answer")
+// }
+// var i=20;
+// var limit=("Enter limit");
+// while (limit>=0){
+// console.log(limit);
+//     limit-=2;
+// }
+
+//
+// var number=prompt("Enter number");
+// var pow=prompt("Enter POW");
+//
+// var result=1;
+// var i=0;
+// while(i<pow){
+//     result*=number;
+//     i++;
+// }
+// console.log(result);
+
+
+// do {
+//     name = prompt("Enter your login name")
+// } while (name != "admin");
+//
+// do {
+//     password = prompt("Enter your password")
+// } while (password != "123123");
+// alert("Hello,admin");
+
+// var limit=prompt("Enter the limit");
+// for(var i=0;i<limit;i+=2){
+//     if (i%4!=0)
+//         continue;
+//         console.log(i);
+// }
+
+var limit = prompt("Enter the limit");
+var flag=0;
+for (var i = 1; i <= limit; i++) {
+    for (var j = 2; j < i; j++) {
+        if(i%j==0) {
+            flag=1;
+            continue;
+        }
+    }
+    if(!flag){
+        console.log(i);
+    }
+}
